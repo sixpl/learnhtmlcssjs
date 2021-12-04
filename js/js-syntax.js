b37e9d41f10cc3e40bc35dd73963f40928aec1ad
sixpl <saket@sixpl.com>
2021-12-05 04:50:41 +0530

    05-Dec-21-03

--- js/js-syntax.js
+++ js/js-syntax.js
@@ -1,7 +1,7 @@
 console.log('Find out the length of this sentence'.length);
 console.log(3-10);
+console.log('I am the new addition.');
 const var1 = 4;
 const var2 = 3;
 const divide1 = var1/var2;
 console.log(divide1);
-console.log('I am the new addition.');
